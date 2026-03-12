6fe3f84094b0c62f776429632b96047637608d89
SenghoutPeng <speng@paragoniu.edu.kh>
2026-03-13 01:16:22 +0700

    Ensure everything works on localhost

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\FrontEndController\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');

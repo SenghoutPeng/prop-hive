0ef145831848300f82acc4eb873bc92e81bff552
SenghoutPeng <speng@paragoniu.edu.kh>
2026-03-13 01:01:34 +0700

    Ensure everything works on localhost

--- api/app/Models/FrontendModel/UtilityRequest.php
+++ api/app/Models/FrontendModel/UtilityRequest.php
@@ -12,16 +12,14 @@ class UtilityRequest extends Model
     protected $table = 'utility_request';
     protected $primaryKey = 'utility_request_id';
 
-    protected $fillable = [
-        'user_id',
-        'id',
-        'description',
-        'status',
-        'priority',
-        'category',
-        'admin_response',
-        'resolved_at',
-    ];
+   protected $fillable = [
+    'utility_request_description',
+    'utility_request_status',
+    'utility_request_created_at',
+    'utility_request_responded_at',
+    'property_id',
+    'user_id'
+];
 
     protected $casts = [
         'resolved_at' => 'datetime',
@@ -36,4 +34,4 @@ class UtilityRequest extends Model
     {
         return $this->belongsTo(Property::class);
     }
-} 
+}
